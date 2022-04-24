445cc7fb7fba85921894083610a474f6be175c86
suriyaprashath <suriyaprashath@gmail.com>
2022-04-24 18:40:26 +0530

    html forms

---
+++ index.js
@@ -0,0 +1,6 @@
+function add() {
+  let number1 = document.getElementById('number1').value;
+  let number2 = document.getElementById('number2').value;
+
+  alert(`Sum of 2 numbers: ${parseInt(number1) + parseInt(number2)}`);
+}
